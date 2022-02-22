d61b5ec14e68813c1e1e1a9f4127fe4ddfd9d1e9
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-02-22 22:21:04 +0100

    updated migration folder

---
+++ src/migrations/20220222211551-alter_user_location_id.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint("users", {
+      fields: ["location_id"],
+      type: "foreign key",
+      name: "location_id_fk", // optional
+      references: {
+        table: "locations",
+        field: "id"
+      },
+      onDelete: "set null",
+      onUpdate: "cascade"
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraints("users", "location_id_fk");
+  }
+};
