08ca4912d8172b1b25fe068cd093711a8d780955
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-02-22 22:14:01 +0100

    added location migration file

--- src/migrations/20220222210839-location.js
+++ src/migrations/20220222210839-location.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(
+      `
+      CREATE TABLE IF NOT EXISTS "locations" (
+        "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
+        "longitude" DECIMAL NOT NULL,
+        "latitude" DECIMAL NOT NULL
+      )
+      `
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DROP TABLE "locations"
+    `);
   }
 };
