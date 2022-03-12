f340edcfebf41d1274dabfdca7528176671fd848
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-03-13 00:28:34 +0100

    add comment for reponse util function ...

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,6 +1,17 @@
 // helper function for returning Api response to client
 
 module.exports = ({ status, msg, data, errorData, code }) => {
+  /**
+   * status:
+   *  - false (an error occured)
+   *  - true (no error occured)
+   * data:
+   *  - associated response data
+   * code:
+   *  -  application error codes
+   * errorData:
+   *  - an array of error if any
+   */
   status = status == undefined ? true : status;
   msg = msg == undefined ? "" : msg;
   data = data == undefined ? {} : data;
