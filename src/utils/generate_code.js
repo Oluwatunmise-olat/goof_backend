876aafaab392bfab028202b14a858e71d93cb91c
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-03-17 13:48:11 +0100

    add reset_code function

--- src/utils/generate_code.js
+++ src/utils/generate_code.js
@@ -3,3 +3,7 @@ const crypto = require("crypto");
 module.exports = () => {
   return crypto.randomBytes(40).toString("hex");
 };
+
+exports.reset_code = () => {
+  return crypto.randomBytes(32).toString("base64").split("=")[0];
+};
