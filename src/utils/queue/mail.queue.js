00ae81f645c20deb3dc7cb0b060c0def555f6067
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-03-19 04:50:15 +0100

    add email queue

---
+++ src/utils/queue/mail.queue.js
@@ -0,0 +1,38 @@
+const config = require("config");
+const REDIS_URL = config.get("redis.url");
+
+const emailQueue = new require("bull")("Email", { redis: REDIS_URL });
+const mail = require("../../service/email.service");
+
+emailQueue.process("reset", async (job) => {
+  console.log(job);
+  const { data } = job;
+  await mail.code_reset_mail(data.to, data.subject, data.token);
+});
+
+emailQueue.process("verify", async (job) => {
+  await mail.verify_email();
+});
+
+emailQueue.process("welcome", async (job) => {
+  await mail.welcome_mail();
+});
+
+emailQueue.process("vendor", async (job) => {
+  await mail.vendor_status_mail();
+});
+
+module.exports = class EmailQueue {
+  static async verifyMail(data) {
+    await emailQueue.add("verify", data);
+  }
+  static async resetMail(data) {
+    await emailQueue.add("reset", data);
+  }
+  static async welcomeMail(data) {
+    await emailQueue.add("welcome", data);
+  }
+  static async vendorStatusMail(data) {
+    await emailQueue.add("vendor", data);
+  }
+};
