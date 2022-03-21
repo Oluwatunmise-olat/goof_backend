9aad7968fca82049227d63c9afcd1e42975ab9fb
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-03-21 11:13:03 +0100

    fix bug in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
-const router = require(express).Router();
+const router = require("express").Router();
 const controller = require("../controllers/user.controller");
 const IsAuth = require("../middleware/auth.middleware");
 
 router.get("/profile", IsAuth, controller.getUserProfileHandler);
+
+module.exports = router;
