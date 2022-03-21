5a663f827b404309dccd8d10e3f56c0d9f84fd12
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-03-21 11:09:39 +0100

    add user routes middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const accountRouter = require("./src/routes/account.routes");
+const userRouter = require("./src/routes/user.routes");
 const oauthRouter = require("./src/routes/webhook.routes");
 
 /**
@@ -10,6 +11,7 @@ const oauthRouter = require("./src/routes/webhook.routes");
 
 app.use(express.json());
 app.use("/api/auth", accountRouter);
+app.use("/api/users", userRouter);
 app.use("/webhook", oauthRouter);
 
 module.exports = app;
