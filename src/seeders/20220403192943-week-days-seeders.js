e46dc550ba9a7c50ae0c49faf31a05026141defd
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-04-03 21:41:38 +0100

    add week days seeder

--- src/seeders/20220403192943-week-days-seeders.js
+++ src/seeders/20220403192943-week-days-seeders.js
@@ -1,24 +1,31 @@
-'use strict';
+"use strict";
+const moment = require("moment");
+
+let currTime = moment().format();
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("week_days", [
+      { name: "monday", created_at: currTime, updated_at: currTime },
+      { name: "tuesday", created_at: currTime, updated_at: currTime },
+      { name: "wednesday", created_at: currTime, updated_at: currTime },
+      { name: "thursday", created_at: currTime, updated_at: currTime },
+      { name: "friday", created_at: currTime, updated_at: currTime },
+      { name: "saturday", created_at: currTime, updated_at: currTime },
+      { name: "sunday", created_at: currTime, updated_at: currTime }
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DELETE FROM "week_days" WHERE
+      name='monday' OR
+      name='tuesday' OR
+      name='wednesday' OR
+      name='thursday' OR
+      name='friday' OR
+      name='saturday' OR
+      name='sunday'
+    `);
   }
 };
