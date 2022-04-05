c0afc9ebd0d8b9ece17c35801ee1d591555bf996
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-04-05 14:44:48 +0100

    add menu modifiers model and migration file

---
+++ src/models/modifiers.js
@@ -0,0 +1,53 @@
+module.exports = (sequelize, DataTypes) => {
+  const modifiers = sequelize.define(
+    "modifiers",
+    {
+      description: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+        required: true
+      },
+      required: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+      },
+      catgeory_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "menu_categories",
+          fields: ["id"]
+        },
+        onDelete: "cascade",
+        onUpdate: "cascade"
+      },
+      min_selection: {
+        type: DataTypes.INTEGER,
+        allowNull: false.valueOf,
+        required: true
+      },
+      max_selection: {
+        type: DataTypes.INTEGER,
+        allowNull: false.valueOf,
+        required: true
+      }
+    },
+    {
+      underscored: true,
+      timestamps: true,
+      modelName: "modifiers",
+      tableName: "modifiers"
+    }
+  );
+
+  modifiers.associate = (models) => {
+    modifiers.belongsTo(models.menu_categories, {
+      foreignKey: "catgeory_id",
+      sourceKey: "catgeory_id",
+      targetKey: "id",
+      as: "modifiers"
+    });
+  };
+  return modifiers;
+};
